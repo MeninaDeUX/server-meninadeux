5f34b58206ed5d3ca7bf123152419d5e4a83e33d
Joao Teles <joaojoseteles@gmail.com>
2023-10-17 15:34:37 -0300

    :sparkles: feat: use-case de registar usuario

---
+++ src/use-cases/register.ts
@@ -0,0 +1,37 @@
+import { UsersRepository } from '@/repositories/users-respository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './erros/user-already-exists-error'
+import { User } from '@prisma/client'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+interface RegisterUseCaseReponse {
+  user: User
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseReponse> {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+    return { user }
+  }
+}
