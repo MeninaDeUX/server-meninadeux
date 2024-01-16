cc03b66ea1f3d1fa87fbf9bf767bb867b6b31200
Lídia Sarti <lid.sarti@gmail.com>
2024-01-16 19:46:17 -0300

    ✨ feat: criacao rota put user

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { getUsersProfile } from './controllers/get-users-profile'
+import { putUser } from './controllers/put-user'
 import { register } from './controllers/register'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.get('/users', getUsersProfile)
+  app.put('/users/', putUser)
 }
